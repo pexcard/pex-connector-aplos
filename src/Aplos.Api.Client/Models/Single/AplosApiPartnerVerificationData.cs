209f6c67e852f71094a0d4d5f5991891d30f00e3
Ty Baker <tbaker@pexcard.com>
2021-01-28 17:38:31 -0500

    Fix response structure for Aplos API /partners/verify

--- src/Aplos.Api.Client/Models/Single/AplosApiPartnerVerificationData.cs
+++ src/Aplos.Api.Client/Models/Single/AplosApiPartnerVerificationData.cs
@@ -4,10 +4,7 @@ namespace Aplos.Api.Client.Models.Single
 {
     public sealed class AplosApiPartnerVerificationData
     {
-        [JsonProperty("aplos_account_id")]
-        public string AplosAccountId { get; set; }
-
-        [JsonProperty("authorized")]
-        public bool Authorized { get; set; }
+        [JsonProperty("partner_verification")]
+        public AplosApiPartnerVerificationDetails PartnerVerification { get; set; }
     }
 }
