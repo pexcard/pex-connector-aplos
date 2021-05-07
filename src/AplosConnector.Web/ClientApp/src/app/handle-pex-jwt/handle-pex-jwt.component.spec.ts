b0e2935f3446d54c4afd70503db81716d82c48ad
pexcardkarcz <80720274+pexcardkarcz@users.noreply.github.com>
2021-05-07 09:05:16 -0400

    Fix NPM High Security Audit Issues (Karma) (#34)
    * upgrade karma
    
    * fix karma tests
    
    * revert non karma pkg version flags
    
    Co-authored-by: Chris Karcz <ckarcz@gmail.com>

--- src/AplosConnector.Web/ClientApp/src/app/handle-pex-jwt/handle-pex-jwt.component.spec.ts
+++ src/AplosConnector.Web/ClientApp/src/app/handle-pex-jwt/handle-pex-jwt.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { HandlePexJwtComponent } from './handle-pex-jwt.component';
 
@@ -8,9 +10,13 @@ describe('HandlePexJwtComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HandlePexJwtComponent ]
+      declarations: [HandlePexJwtComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [
+        { provide: 'BASE_URL', useValue: 'http://localhost:5001' }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- src/AplosConnector.Web/ClientApp/src/app/sync-history/sync-history.component.spec.ts
+++ src/AplosConnector.Web/ClientApp/src/app/sync-history/sync-history.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { SyncHistoryComponent } from './sync-history.component';
 
@@ -8,9 +10,13 @@ describe('SyncHistoryComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SyncHistoryComponent ]
+      declarations: [SyncHistoryComponent],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      providers: [
+        { provide: 'BASE_URL', useValue: 'http://localhost:5001' }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
