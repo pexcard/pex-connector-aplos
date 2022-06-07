e5f552e37434de477067245c4d43c746916d5301
Chris Karcz <ckarcz@gmail.com>
2022-06-07 12:22:46 -0400

    Aplos - Fetch Transactions in Date Range Batches (#82)
    * batching dates
    
    * 4 week date batches

---
+++ src/AplosConnector.Common/Models/DateTimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AplosConnector.Common.Models
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime AddWeeks(this DateTime date, int weeks)
+        {
+            return date.AddDays(7 * weeks);
+        }
+
+        public static DateTime ToStartOfDay(this DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, date.Day, 00, 00, 00, 00);
+        }
+
+        public static DateTime ToEndOfDay(this DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, date.Day, 23, 59, 59, 999);
+        }
+    }
+}
