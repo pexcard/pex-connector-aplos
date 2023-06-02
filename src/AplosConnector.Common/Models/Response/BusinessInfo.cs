67b3c1fc591a4aebf1874db1ae931cf648795864
vpetryk <vpetryk@softjourn.com>
2023-06-02 17:17:17 +0300

    72381 Aplos: Help icon for credit business is directing use to Aplos for expense businesses article (#139)

---
+++ src/AplosConnector.Common/Models/Response/BusinessInfo.cs
@@ -0,0 +1,10 @@
+﻿using PexCard.Api.Client.Core.Models;
+
+namespace AplosConnector.Common.Models.Response
+{
+    public class BusinessInfo
+    {
+        public string BusinessName { get; set; }
+        public FundingSource FundingSource { get; set; }
+    }
+}
