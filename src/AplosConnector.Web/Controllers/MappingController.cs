7361e9636083102ddb8e9548c9d3c30ece45c9b3
Chris Karcz <ckarcz@pexcard.com>
2023-07-27 10:12:39 -0400

    PartnerVerificationUrl

--- src/AplosConnector.Web/Controllers/MappingController.cs
+++ src/AplosConnector.Web/Controllers/MappingController.cs
@@ -116,7 +116,7 @@ namespace AplosConnector.Web.Controllers
                 HasAplosAccountId = !string.IsNullOrWhiteSpace(mapping.AplosAccountId),
             };
 
-            if (result.AplosAuthenticationMode == AplosAuthenticationMode.PartnerAuthentication && result.HasAplosAccountId)
+            if (result.AplosAuthenticationMode == AplosAuthenticationMode.PartnerAuthentication && !result.HasAplosAccountId)
             {
                 result.PartnerVerificationUrl = _appSettings.AplosPartnerVerificationUrl.ToString();
             }
