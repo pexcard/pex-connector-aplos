4163c5df2f2aedd71c47b7d729ee6b2ed1e7a308
vpetryk <vpetryk@softjourn.com>
2024-02-15 18:56:09 +0200

    84491 Aplos for Charge: Account for business fee trx and cardholder fee trx

---
+++ src/AplosConnector.Common/Const/TransactionCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace AplosConnector.Common.Const
+{
+    public static class TransactionCategory
+    {
+        public static readonly string BusinessFunding = "BusinessFunding";
+        public static readonly string BusinessAccountFee = "BusinessAccountFee";
+        public static readonly string CardAccountFee = "CardAccountFee";
+        public static readonly string Dispute = "Dispute";
+    }
+}
