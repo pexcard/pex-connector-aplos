4ec1058b5b2c0747e2da22f1dfd6eb9838f188eb
Chris Karcz <ckarcz@pexcard.com>
2025-04-02 14:07:46 -0400

    fix tag sync history row key bug

--- src/AplosConnector.Common/Storage/SyncHistoryStorage.cs
+++ src/AplosConnector.Common/Storage/SyncHistoryStorage.cs
@@ -1,16 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using AplosConnector.Common.Entities;
+﻿using AplosConnector.Common.Entities;
 using AplosConnector.Common.Models;
 using Azure.Data.Tables;
+using Murmur;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AplosConnector.Common.Storage
 {
     public class SyncHistoryStorage : AzureTableStorageAbstract
     {
+        private static readonly HashAlgorithm _hashAlgo = MurmurHash.Create32();
+
         public const string TABLE_NAME = "SyncHistory";
 
         public SyncHistoryStorage(TableClient tableClient) : base(tableClient) { }
@@ -20,9 +25,21 @@ namespace AplosConnector.Common.Storage
             var entity = new SyncHistoryEntity(model)
             {
                 PartitionKey = model.PEXBusinessAcctId.ToString(),
-                RowKey = $"{model.PEXBusinessAcctId}-{model.SyncType}-{model.CreatedUtc.Ticks}"
+                RowKey = GenerateRowKey(model),
             };
             await TableClient.AddEntityAsync(entity, cancellationToken);
+        }
+
+        private string GenerateRowKey(SyncResultModel model)
+        {
+            // should generate a short unique id
+
+            var uniqueId = $"{model.SyncType}-{model.CreatedUtc.Ticks}";
+            var bytes = Encoding.UTF8.GetBytes(uniqueId);
+            var hash = _hashAlgo.ComputeHash(bytes);
+            var base64Hash = Convert.ToBase64String(hash);
+
+            return base64Hash;
         }
 
         public async Task<List<SyncResultModel>> GetByBusiness(int businessAcctId, CancellationToken cancellationToken)
