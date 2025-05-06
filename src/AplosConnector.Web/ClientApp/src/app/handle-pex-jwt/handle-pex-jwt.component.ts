fd7bf35840596eec6bdd1e8fee7617c6fa5d7bbf
Chris Karcz <ckarcz@pexcard.com>
2025-05-06 12:14:15 -0400

    improve JWT transfer component

--- src/AplosConnector.Web/ClientApp/src/app/handle-pex-jwt/handle-pex-jwt.component.ts
+++ src/AplosConnector.Web/ClientApp/src/app/handle-pex-jwt/handle-pex-jwt.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { concatMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-handle-pex-jwt',
@@ -8,23 +9,31 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./handle-pex-jwt.component.css']
 })
 export class HandlePexJwtComponent implements OnInit {
+  loginFailed = false;
 
   constructor(private activatedRoute: ActivatedRoute, private router: Router, private auth: AuthService) { }
 
   ngOnInit() {
-    this.activatedRoute.queryParamMap.subscribe(params => {
-      const jwt = params.get('jwt');
-      this.auth.exchangeJWTForSession(jwt).subscribe(
-        tokenModel =>{
-          console.log('got session', tokenModel);
-          const session = tokenModel.token;
-          this.auth.login(session).subscribe(() => {
-            console.log('navigating away');
-            this.router.navigate(['connect']);
-          });
+    const jwt = this.activatedRoute.snapshot.queryParams.jwt;
+    this.auth.exchangeJWTForSession(jwt)
+      .pipe(
+        concatMap((token) => this.auth.login(token.token)),
+        concatMap(() => this.auth.headlessMode),
+      ).subscribe({
+        next: (headlessMode) => {
+          let segments: string[] = [];
+          if (headlessMode) {
+            segments.push('headless');
+          }
+          segments.push('connect');
+
+          void this.router.navigate(segments);
+        },
+        error: (error) => {
+          console.error('Error handling PEX jwt login:', error);
+          this.loginFailed = true;
         }
-      );
-    });
+      });
   }
 
 }
